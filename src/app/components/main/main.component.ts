40ec919f27bea80da58e10a9716967e0663b8195
assylzhan <assylzhan.kizbayev@gmail.com>
2021-02-26 01:39:47 +0600

    todo replay data

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
-import { Observable } from 'rxjs';
+import { ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { WebSocketService } from '../../services/web-socket.service';
 
 @Component({
@@ -9,30 +9,47 @@ import { WebSocketService } from '../../services/web-socket.service';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-  user$!: Observable<any>;
-  user: any;
+  user$ = new ReplaySubject(8);
   currentUserId = 0;
   totalUserAmount = 0;
+  data = [1, 2, 3, 4, 5];
 
   constructor(private webSocket: WebSocketService) { }
 
-  ngOnInit(): void {
-    this.webSocket.listen('connect').subscribe( () => console.log('Connection succeeded') );
+  // get user() {
+  //   return this.user$.asObservable().pipe(
+  //     map(item => item)
+  //   );
+  // }
 
-    this.webSocket.listen('user-info').subscribe( data => {
+  ngOnInit(): void {
+    this.webSocket.listen('connect').subscribe(() => console.log('Connection succeeded'));
+
+    this.webSocket.listen('user-info').subscribe(data => {
       this.currentUserId = data.id;
-      console.log('Current user id', this.currentUserId);
+      // console.log('Current user id', this.currentUserId);
     });
 
-    this.webSocket.listen('registered-users-amount').subscribe( data => {
+    this.webSocket.listen('registered-users-amount').subscribe(data => {
       this.totalUserAmount = data.amount;
-      console.log('Accept total users amount', this.totalUserAmount);
+      // console.log('Accept total users amount', this.totalUserAmount);
     });
 
     // this.user$ = this.webSocket.listen('user-info');
-    this.webSocket.listen('user-info').subscribe( data => {
-      this.user = data.id;
+    this.webSocket.listen('user-info').subscribe(data => {
+      // this.user = data.id;
+      console.log('************save', data.id);
+
+      this.user$.next(data.id)
+
+      this.user$.subscribe(val => {
+        console.log('replay', val);
+      });
     });
   }
+}
 
+interface User {
+  id: number;
+  room: number;
 }
